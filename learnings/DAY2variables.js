4756156b38abc1b004be7ea686878aadb85bbb31
KARTIK RAJENDRA PHAD <117894918+kartik-phad@users.noreply.github.com>
2025-03-21 19:12:54 +0000

    DAY 2

---
+++ learnings/DAY2variables.js
@@ -0,0 +1,13 @@
+let name = "kartik";
+let age = 21;
+let isadult =  true;
+let state;
+
+// number => 2 to power 53
+//string = ""
+// null = standalone value
+// undefined =>
+// symbol =    unique
+
+console.log(typeof null); // gives output as object
+console.log(typeof null); // object
