2df2e091542da4a1e58b8729fddcb5ad9c70f65d
KARTIK RAJENDRA PHAD <117894918+kartik-phad@users.noreply.github.com>
2025-03-22 05:51:41 +0000

    TypeConversion

---
+++ learnings/DAY3TypeConversion.js
@@ -0,0 +1,37 @@
+// let score = 34;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let NumberType = Number(score);
+
+// let score = "abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let NumberType = Number(score);
+
+
+// let score = "34ABC";
+// console.log(typeof score);
+// console.log(score)
+// let NumberType = Number(score);
+// console.log(NumberType)
+
+// ==================================================================================================
+
+// let isloggedin = 1;
+
+// let booleanloggedin = Boolean(isloggedin);
+// console.log(booleanloggedin);
+
+// 1 = true ; 2 = false
+// "" => false
+// "kartik" => True
+
+//============================================================================================
+
+let somennumber = 8
+let Stringnumber =  String(somennumber) // number type
+console.log(typeof Stringnumber)        //string type
