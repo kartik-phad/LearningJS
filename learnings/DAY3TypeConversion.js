22d002da1d8d9ec231e502eac39add35c188a592
KARTIK RAJENDRA PHAD <117894918+kartik-phad@users.noreply.github.com>
2025-03-22 07:02:27 +0000

    data types

--- learnings/DAY3TypeConversion.js
+++ learnings/DAY3TypeConversion.js
@@ -32,6 +32,39 @@
 
 //============================================================================================
 
-let somennumber = 8
-let Stringnumber =  String(somennumber) // number type
-console.log(typeof Stringnumber)        //string type
+// let somennumber = 8
+// let Stringnumber =  String(somennumber) // number type
+// console.log(typeof Stringnumber)        //string type
+
+
+
+//================================== OPEARTIONS  =============================================
+
+let value =3;
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**2);   BASIC MATHS OPERATIONS
+
+// ===========================================
+// let str1 = "Hello"
+// let str2 = " kartik"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2);       //12
+// console.log(1 + "2");       //12
+// console.log("1" + 2 + 2);       //122
+// console.log(1 + 2 + "2");       //32
+
+let x = 5;
+const y = x++;
+console.log(y)
+
+let x2 = 3n;
+const y2 = x2++;
+
