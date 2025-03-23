93b99144ea7437d55f3e70b90ac271befdab47be
KARTIK RAJENDRA PHAD <117894918+kartik-phad@users.noreply.github.com>
2025-03-23 04:38:42 +0000

    o2 basics

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,31 @@
+
+myArray = [1,2,3,4,5]
+
+// console.log(myArray.indexOf(3));        // returnes index
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop(7);
+// console.log(myArray)
+
+
+// myArray.unshift(9);         //new element oush at index 0[2]
+// console.log(myArray)
+// // myArray.shift();            // removs first elemtnt
+// console.log(myArray);
+
+
+
+
+// myArray1 = console.log(`original array ${myArray} `)
+// // console.log(typeof myArray1)
+// console.log(myArray.slice(1,3));            // no changes in original array returns array sliced
+
+// console.log(myArray.splice(1,3))        //Changes are made in origrinal array and returned [shaalo copy]
+// myArray1 = console.log(`original array after using splice method  ${myArray} `)
+
+
+// =======================  NOTE =======================
+
+// => slice  dont manipulate original Array
+// => splice manipulate original Array
